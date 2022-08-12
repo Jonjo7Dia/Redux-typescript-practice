b0adfe73f5469fff976e516ef61ef8c48203d75b
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-11 21:16:32 -0300

    ready to use

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "./store";
 
 function App() {
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const balance = useSelector((state: State) => state.bank);
   return (
     <div className="App">
+      <h1>{balance}</h1>
+      <button onClick={() => {depositMoney(1000)}}>Deposit</button>
+      <button onClick={() => {withdrawMoney(100)}}>Withdraw</button>
+      <button onClick={() => {bankrupt()}}>Bankrupt</button>
     </div>
   );
 }
